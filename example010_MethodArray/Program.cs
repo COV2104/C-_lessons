2f9809bbcae283fbba096699792dc7ba29ec9376
COV <tsehlo@mail.ru>
2022-10-02 13:50:13 +0300

    example010 MethodArray

---
+++ example010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int []array = {15,26,54,78,12,36,45,79,25,46};
+int n= array.Length;
+int find = 36;
+int index =0;
+while(index<n)
+{
+    if(array[index]==find)
+    {
+        System.Console.WriteLine($"Индекс искомого числа = {index}");
+        break;
+    }
+    index++;
+}
