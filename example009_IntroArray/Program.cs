1d9773ec9aeae3f85df8cca84f9dfd9de14bc467
COV <tsehlo@mail.ru>
2022-10-02 13:41:03 +0300

    example009 IntroArray

---
+++ example009_IntroArray/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти максимум в массиве из 9 элементов.
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int [] array = {36,45,59,96,47,78,12,3,4};
+
+int max = Max(Max(array[0], array[1], array[2]),Max(array[3], array[4], array[5]),Max(array[6], array[7], array[8]));
+
+Console.WriteLine($"Максимум = {max}");
