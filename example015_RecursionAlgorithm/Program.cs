3280f84ef296e77d0ff73c8bacae05b8d1157b23
COV <tsehlo@mail.ru>
2022-10-16 19:26:29 +0300

    example015 RecursionAlgorithm

---
+++ example015_RecursionAlgorithm/Program.cs
@@ -0,0 +1,39 @@
+﻿// string [,] table = new string [2,5];
+// table [1,2] = "слово";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.Write($"{table[rows, columns]}, \t");
+//     }
+//     Console.WriteLine();
+// }
+
+
+void PrintArray(int[,] array)
+{
+    for (int rows = 0; rows < array.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < array.GetLength(1); columns++)
+        {
+            Console.Write($"{array[rows, columns]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,100);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 5];
+FillArray(matrix);
+PrintArray(matrix);
